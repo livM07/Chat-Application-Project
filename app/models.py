a132d2933204bc3624ee8f0ed497b67ed1481348
livM07 <132204395+livM07@users.noreply.github.com>
2023-05-14 00:51:25 +0800

    create account page
    changed DB schema to include email in the users table.
    added functionality for create account to create account page.

--- app/models.py
+++ app/models.py
@@ -2,8 +2,9 @@ from app import db
 
 class user(db.Model):
     userID = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False)
-    password = db.Column(db.Text, nullable=False)
+    name = db.Column(db.String(70), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         return 'User: {}'.format(self.name, self.password)
@@ -11,7 +12,7 @@ class user(db.Model):
 # will store messages as a JSON object in conv_json
 class conversations(db.Model):
     conversationID = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.Text, nullable=False)
+    title = db.Column(db.String(150), nullable=False)
     conv_json = db.Column(db.Text)
     userID = db.Column(db.Integer, db.ForeignKey('user.userID'), nullable=False)
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
-from app import app
-
+from flask import render_template, jsonify, session, redirect, request, url_for, flash
+from app import app, db
+from app.models import user, conversations
+import json
 
 # CHANGED STATIC FOLDER -> TEMPLATES FOR TESTING. ADD STATIC BACK LATER.
 @app.route('/')
@@ -17,8 +18,46 @@ def chat():
 
 @app.route('/create', methods = ['GET','POST'])
 def create():
-    return "create account here"
+    if request.method == "POST":
+        # TO DO -> store form data in session
+        username = request.form["username"]
+        session["username"] = username
+        email = request.form["email"]
+        session["email"] = email
+        password = request.form["password"]
+        session["password"] = password
+
+        # TO DO -> check if email is already in DB
+        email_exists = user.query.filter_by(email=email).first()
+        if email_exists:
+            flash("email already in use")
+        else: 
+            new_user = user(name=username, email=email, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            return "successfully created new user"
+    else:
+        return render_template("create.html")
 
 @app.route('/login', methods = ['GET','POST'])
 def login():
-    return "login here"
+    if "username" in session:
+        render_template("history-page.html")
+    else:
+        render_template("login.html")
+        #  TO DO -> check login data (exists + is correct)
+        email_exists = user.query.filter_by(email=email).first()
+        if email_exists:
+            # TO DO -> check password is correct
+            # if data is correct -> create session + render history page
+            if request.method == "POST":
+                email = request.form["email"]
+                session["email"] = email
+                password = request.form["password"]
+                session["password"] = password
+                render_template("history-page.html")
+
+@app.route('/logout')
+def logout():
+     session.pop("username", None)
+     return "logged out"
