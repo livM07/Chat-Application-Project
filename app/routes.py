557eb22e8101a3d64a6ebc576f9cb556ccca63d8
livM07 <132204395+livM07@users.noreply.github.com>
2023-05-13 16:25:59 +0800

    migrations directory + base template
    created a migrations directory and new DB based on models.
    created a templates folder and base template - feel free to add stuff to base template which all pages will have.

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,24 @@
 from flask import render_template
 from app import app
 
-@app.route('/')
-def index():
-    return "Hello World!"
 
-@app.route('/create', methods = ['POST'])
+# CHANGED STATIC FOLDER -> TEMPLATES FOR TESTING. ADD STATIC BACK LATER.
+@app.route('/')
+def home():
+    return render_template("base.html", title="base")
+
+@app.route('/history')
+def history():
+      return render_template("history-page.html")
+
+@app.route('/chat')
+def chat():
+      return render_template("chat-page.html")
+
+@app.route('/create', methods = ['GET','POST'])
 def create():
     return "create account here"
 
-@app.route('/login', methods = ['POST'])
+@app.route('/login', methods = ['GET','POST'])
 def login():
     return "login here"
