0cc20333e1d3b8aa01b1b1127499d660aca45ff9
L0n35 <lachlanrjones@gmail.com>
2023-05-20 18:08:28 +0800

    added working TravelBot

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,17 @@ from flask import render_template, jsonify, session, redirect, request, url_for,
 from app import app, db
 from app.forms import LoginForm, CreateForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import user
+from app.models import user, conversations
 import json
+import openai
+from sqlalchemy import select
+
+# OpenAI API key
+openai.api_key = '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'
+# OpenAI model 
+MODEL_NAME = 'gpt-3.5-turbo'
+
+
 
 # CHANGED STATIC FOLDER -> TEMPLATES FOR TESTING. ADD STATIC BACK LATER.
 @app.route('/')
@@ -17,11 +26,102 @@ def home():
 @app.route('/history')
 @login_required
 def history():
-      return render_template("history-page.html")
+      
+
+
+
+
+      return render_template("history-page.html", name=currentUserName)
 
 @app.route('/chat')
-def chat():
-      return render_template("chat-page.html")
+def new_chat():
+      empty_json = {
+        "messages": []
+      }
+      conversation = conversations(title="", conv_json=json.dumps(empty_json), userID=currentUserID)
+      db.session.add(conversation)
+      db.session.commit()
+      global conversationID
+      conversationID = conversation.get_id()
+      return render_template("chat.html", name=currentUserName)
+
+@app.route('/chat/<id>')
+def chat(id):
+      # todo: add check to see if id exists
+      global conversationID
+      conversationID = id
+      return render_template("chat.html")
+     
+    
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    location = request.form['location']
+    message = request.form['message']
+
+    # Prepare the conversation input
+    conversation = [
+        {'role': 'system', 'content': 'You are a helpful assistant who helps users with travel related questions about ' + location + ' only.'},
+        {'role': 'user', 'content': 'In 50 words or less answer the question: ' + message}
+    ]
+
+    # Generate TravelBots's response using stream
+    response = openai.ChatCompletion.create(
+        model=MODEL_NAME,
+        messages=conversation,
+        max_tokens=200,
+        n=1,
+        stop=None
+    )
+    
+    reply = response.choices[0].message.content
+
+    conversationQuery = conversations.query.filter_by(conversationID=conversationID).first()
+    conversationJSON = conversationQuery.get_json()
+    conversationMessages = json.loads(conversationJSON)
+    print(conversationMessages)
+
+    conversationMessages["messages"].append({"author": current_user.name, "message": message})
+    conversationMessages["messages"].append({"author": "TravelBot", "message": reply})
+    print(conversationMessages)
+
+    conv = conversations.query.filter_by(conversationID=conversationID).first()
+    conv.set_title(location)
+    conv.set_json(json.dumps(conversationMessages))
+    db.session.commit()
+
+    print(conv.get_json())
+
+    
+
+    # messages = {
+    #     "messages": [
+    #         {"author": current_user.name, "message": message},
+    #         {"author": "TravelBot", "message": reply}
+    #     ]
+    # }
+
+    # print(messages)
+    # m = json.dumps(messages)
+    # print(m) 
+
+    
+    
+    return reply
+
+@app.route('/get_conversations', methods=['POST'])
+def get_conversations():
+    #location = request.form['location']
+    print('currentID',currentUserID)
+    #convs = conversations.query.filter_by(userID=currentUserID)
+    convs = select(conversations).where(conversations.userID == currentUserID)
+    
+    print(convs)
+
+    return "jeff"
+
+    
+
+
 
 # WORKS!!
 @app.route('/create', methods = ['GET','POST'])
@@ -50,7 +150,12 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
+          global currentUserID
+          global currentUserName
           currentUser = user.query.filter_by(email=form.email.data).first()
+          currentUserID = currentUser.get_id()
+          print('currentuserid',currentUserID)
+          currentUserName = currentUser.get_name()
           if currentUser is None:
             flash('email is not in DB')
             return redirect(url_for('login'))
