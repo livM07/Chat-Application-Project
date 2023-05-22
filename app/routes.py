e321572b1df9c4782d41ed7bd5834068780cbad6
livM07 <132204395+livM07@users.noreply.github.com>
2023-05-23 01:22:58 +0800

    merge conflict

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ import openai
 from sqlalchemy import select
 
 # OpenAI API key
-openai.api_key = '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'
+openai.api_key = ''
 # OpenAI model 
 MODEL_NAME = 'gpt-3.5-turbo'
 
